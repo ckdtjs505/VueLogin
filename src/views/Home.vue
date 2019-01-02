10fe03e72efc78ce8bf87ae5500d0cec9b9e0413
ckdtjs505 <36597179+ckdtjs505@users.noreply.github.com>
2019-01-02 16:56:46 +0900

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img id="thumbnails" alt="Vue logo" src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Login/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Login from "@/components/Login.vue";
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    Login
+  }
+};
+</script>
+
+<style media="screen">
+@media screen and (max-width: 992px) {
+  #thumbnails {
+     width: 70%;
+  }
+}
+</style>
---
+++ src/views/Test.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="test">
+    안녕하세요 오창선입니다.<br>
+    간단한 테스트 페이지 입니다
+  </div>
+</template>
